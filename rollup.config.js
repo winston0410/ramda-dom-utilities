d9b7ee5fccb7de5ab149d7e3fdd804edf731028e
Hugo <hugosum@PC3343.local>
2020-08-28 17:55:47 +0800

    :wrench: Update Rollup.js config

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import multiInput from 'rollup-plugin-multi-input'
 const {
   terser
 } = require('rollup-plugin-terser')
@@ -8,27 +7,26 @@ const {
 const commonjs = require('@rollup/plugin-commonjs')
 
 export default [{
-  input: './static/js/*.js',
+  input: './index.js',
   output: [{
     dir: 'dist',
     format: 'esm',
     plugins: [
-      // terser({
-      //   compress: {
-      //     drop_console: true
-      //   },
-      //   output: {
-      //     comments: false
-      //   },
-      //   ecma: 2019
-      // })
+      terser({
+        compress: {
+          drop_console: true
+        },
+        output: {
+          comments: false
+        },
+        ecma: 2019
+      })
     ]
   }],
   plugins: [
     nodeResolve({}),
     commonjs({
       include: ['./src/**', 'node_modules/**']
-    }),
-    multiInput({ relative: './static/' })
+    })
   ]
 }]

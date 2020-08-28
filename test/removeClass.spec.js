0a59693d9904a67c6a0381769e3599b2e622670f
kghugo <howtostudycantonese@gmail.com>
2020-08-28 21:35:02 +0800

    :wrench: Update ESLint config

--- test/removeClass.spec.js
+++ test/removeClass.spec.js
@@ -1,5 +1,6 @@
 const chai = require('chai')
 const expect = chai.expect
+require('basichtml').init()
 
 describe('removeClass()', function () {
   it('should remove a class from element', function () {

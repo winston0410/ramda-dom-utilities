d5ab074dc731ac71482c72f720ab2f835e1086b7
kghugo <howtostudycantonese@gmail.com>
2020-08-29 01:15:40 +0800

    :white_check_mark: Add test for addClass()

---
+++ test/addClass.spec.js
@@ -0,0 +1,28 @@
+const {
+  addClass
+} = require('../index.js')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('chai-dom'))
+const R = require('ramda')
+require('basichtml').init()
+
+describe('addClass()', function () {
+  let element, returnedValue
+
+  beforeEach(function () {
+    element = document.createElement('p')
+
+    returnedValue = R.pipe(
+      addClass('test-class')
+    )(element)
+  })
+
+  it('should add a class to an element', function () {
+    expect(returnedValue).to.have.class('test-class')
+  })
+
+  it('should return the element itself', function () {
+    expect(returnedValue).to.eql(element)
+  })
+})

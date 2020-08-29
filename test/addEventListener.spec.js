0d224681fb355efb55edf7cb34b799265778b537
kghugo <howtostudycantonese@gmail.com>
2020-08-29 15:27:22 +0800

    :white_check_mark: Create test for addEventListener()

---
+++ test/addEventListener.spec.js
@@ -0,0 +1,28 @@
+const {
+  addEventListener
+} = require('../index.js')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('chai-dom'))
+const R = require('ramda')
+
+describe('addEventListener()', function () {
+  let element, returnedValue
+
+  beforeEach(function () {
+    require('basichtml').init()
+    element = document.createElement('p')
+
+    returnedValue = R.pipe(
+      addEventListener('click')
+    )(element)
+  })
+
+  it('should add an event listener to an element', function () {
+    // expect(returnedValue).to.have.class('test-class')
+  })
+
+  it('should return the element itself', function () {
+    expect(returnedValue).to.eql(element)
+  })
+})

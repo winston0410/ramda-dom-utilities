5651912febe1dbfa7c16e09a73317debb642db18
kghugo <howtostudycantonese@gmail.com>
2020-08-29 18:12:56 +0800

    :construction: Create test suite for getElementById()

---
+++ test/getElementById.spec.js
@@ -0,0 +1,23 @@
+// const {
+//   getElementById
+// } = require('../index.js')
+// const chai = require('chai')
+// const expect = chai.expect
+// chai.use(require('chai-dom'))
+// const R = require('ramda')
+//
+// describe('getElementById()', function () {
+//   let element, returnedValue
+//
+//   beforeEach(function () {
+//     require('basichtml').init()
+//     element = document.createElement('p')
+//     document.body.append(element)
+//
+//     returnedValue = getElementById('p')(document)
+//   })
+//
+//   it('should return the selected element', function () {
+//     expect(returnedValue).to.equal(element)
+//   })
+// })

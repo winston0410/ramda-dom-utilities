52fdb21e6e4d0d398efb48345b5ca7c222e3076a
kghugo <howtostudycantonese@gmail.com>
2020-08-29 09:51:42 +0800

    :sparkles: Create setProp() to turn setting properties(e.g. textContent) a function)

---
+++ test/setProp.spec.js
@@ -0,0 +1,30 @@
+const {
+  setProp
+} = require('../index.js')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('chai-dom'))
+const R = require('ramda')
+require('basichtml').init()
+
+describe('setProp()', function () {
+  let element, returnedValue
+
+  beforeEach(function () {
+    element = document.createElement('p')
+
+    returnedValue = R.pipe(
+      setProp('textContent')('Example text'),
+      setProp('id')('unique-id')
+    )(element)
+  })
+
+  it('should change properties of an element', function () {
+    expect(returnedValue).to.have.id('unique-id')
+    expect(returnedValue).to.contain.text('Example text')
+  })
+
+  it('should return the element itself', function () {
+    expect(returnedValue).to.eql(element)
+  })
+})

5f5d05915ca2015f2fd78ce5618a4597d9f9f32e
kghugo <howtostudycantonese@gmail.com>
2020-08-29 09:17:09 +0800

    :bug: Fix bug where the name querySelector and querySelectorAll has been swapped

---
+++ test/querySelector.spec.js
@@ -0,0 +1,22 @@
+const {
+  querySelector
+} = require('../index.js')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('chai-dom'))
+const R = require('ramda')
+require('basichtml').init()
+
+describe('querySelector()', function () {
+  let element, returnedValue
+
+  beforeEach(function () {
+    element = document.createElement('p')
+
+    returnedValue = querySelector('p')('document')
+  })
+
+  it('should return the selected element', function () {
+    expect(returnedValue).to.eql(element)
+  })
+})

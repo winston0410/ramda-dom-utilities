145f457f07c7499f1c43fd8e1d8cf423765de96e
kghugo <howtostudycantonese@gmail.com>
2020-08-29 19:02:09 +0800

    :white_check_mark: Complete test for getElementById()

--- test/getElementById.spec.js
+++ test/getElementById.spec.js
@@ -1,23 +1,30 @@
-// const {
-//   getElementById
-// } = require('../index.js')
-// const chai = require('chai')
-// const expect = chai.expect
-// chai.use(require('chai-dom'))
-// const R = require('ramda')
-//
-// describe('getElementById()', function () {
-//   let element, returnedValue
-//
-//   beforeEach(function () {
-//     require('basichtml').init()
-//     element = document.createElement('p')
-//     document.body.append(element)
-//
-//     returnedValue = getElementById('p')(document)
-//   })
-//
-//   it('should return the selected element', function () {
-//     expect(returnedValue).to.equal(element)
-//   })
-// })
+const {
+  getElementById,
+  createElement,
+  setAttribute
+} = require('../index.js')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('chai-dom'))
+const R = require('ramda')
+
+describe('getElementById()', function () {
+  let element, returnedValue
+
+  beforeEach(function () {
+    require('basichtml').init()
+
+    element = R.pipe(
+      createElement('p'),
+      setAttribute('id')('test-id')
+    )(document)
+
+    document.body.append(element)
+
+    returnedValue = getElementById('test-id')(document)
+  })
+
+  it('should return the selected element', function () {
+    expect(returnedValue).to.equal(element)
+  })
+})

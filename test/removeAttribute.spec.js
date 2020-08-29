b7220287cacbb336fa0a11d3031932d265610163
kghugo <howtostudycantonese@gmail.com>
2020-08-29 09:43:59 +0800

    :white_check_mark: Add test for removeAttribute()

---
+++ test/removeAttribute.spec.js
@@ -0,0 +1,30 @@
+const {
+  removeAttribute,
+  setAttribute
+} = require('../index.js')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('chai-dom'))
+const R = require('ramda')
+require('basichtml').init()
+
+describe('setAttribute()', function () {
+  let element, returnedValue
+
+  beforeEach(function () {
+    element = document.createElement('p')
+
+    returnedValue = R.pipe(
+      setAttribute('id', 'unique'),
+      removeAttribute('id')
+    )(element)
+  })
+
+  it('should remove an attribute from the element', function () {
+    expect(returnedValue).to.not.have.attr('id', 'unique')
+  })
+
+  it('should return the element itself', function () {
+    expect(returnedValue).to.eql(element)
+  })
+})

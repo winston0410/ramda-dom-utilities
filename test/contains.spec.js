d1292dd4f09478f34253f2fc91d78ea10514545c
Hugo <hugosum@PC3343.local>
2020-08-31 12:18:44 +0800

    :white_check_mark: Add test for contains()

---
+++ test/contains.spec.js
@@ -0,0 +1,36 @@
+const {
+  contains,
+  append
+} = require('../index.js')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('chai-dom'))
+const R = require('ramda')
+
+describe('contains()', function () {
+  let element, elementToAppend, elementToNotAppend
+
+  beforeEach(function (done) {
+    const jsdom = require('jsdom')
+    const { JSDOM } = jsdom
+    const { document } = (new JSDOM('...')).window
+
+    element = document.createElement('p')
+
+    document.body.append(element)
+
+    elementToAppend = document.createElement('span')
+
+    elementToNotAppend = document.createElement('div')
+
+    append(elementToAppend)(element)
+    append(elementToNotAppend)(document.body)
+
+    done()
+  })
+
+  it('should return whether the element contains a node', function () {
+    expect(contains(elementToAppend)(element)).to.be.true
+    expect(contains(elementToNotAppend)(element)).to.be.false
+  })
+})
